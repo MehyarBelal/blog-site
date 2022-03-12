d7572487d056fd71d69923207b1bdbf75de753f8
s3559505 <53518501+s3559505@users.noreply.github.com>
2022-03-12 21:55:22 +1100

    m

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 
+let bars = document.getElementById('bars');
+let sidebar = document.querySelector('.sidebar');
+bars.addEventListener("click", openMenu);
+
+
+
+function openMenu(){
+  
+    $('.sidebar').show();
+sidebar.classList.add('open-bar')
+
+}

a55e47a1f1b666d1bc035e278c3f79d210565f18
s3559505 <53518501+s3559505@users.noreply.github.com>
2022-03-12 22:23:23 +1100

    m

--- app.js
+++ app.js
@@ -3,11 +3,21 @@ let bars = document.getElementById('bars');
 let sidebar = document.querySelector('.sidebar');
 bars.addEventListener("click", openMenu);
 
-
+$('#btnclose').hide();
+$('.sidebar').hide();
 
 function openMenu(){
-  
+    sidebar.classList.remove('close-bar')
     $('.sidebar').show();
 sidebar.classList.add('open-bar')
-
+$('#btnclose').show();
 }
+
+
+$('#btnclose').click(function(){
+    sidebar.classList.remove('open-bar')
+
+
+   sidebar.classList.add('close-bar');
+   $('#btnclose').hide();
+})

0538dce61abd9cce6b0ff4e4894955f225155fc6
s3559505 <53518501+s3559505@users.noreply.github.com>
2022-03-13 16:29:27 +1100

    m

--- app.js
+++ app.js
@@ -21,3 +21,21 @@ $('#btnclose').click(function(){
    sidebar.classList.add('close-bar');
    $('#btnclose').hide();
 })
+
+
+$('#user').hover(function(){
+    $('#user').removeClass('fa-user')
+
+    $('#user').addClass('fa-arrow-right-to-bracket ')
+
+
+    $('#user').click(function(){
+        window.location.href = "signin.html"
+    })
+})
+
+$('#user').mouseout(function(){
+    $('#user').removeClass('fa-arrow-right-to-bracket')
+
+    $('#user').addClass('fa-user ')
+})
